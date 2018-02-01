d410f830316f6e1722522708e336bb4f63065ce4
Jesús Mª <jeluchu@gmail.com>
2018-02-01 09:49:22 +0100

    RAÍZ CUADRADA

---
+++ 02_raices/raizcuad.cpp
@@ -0,0 +1,36 @@
+//PROGRAMA EN C++
+//AUTOR: JESÚS MARÍA CALDERÓN - GITHUB JELUCHU
+//https://github.com/Jeluchu
+
+// LIBRERÍAS
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <complex.h>
+
+
+// DEFINICIÓN
+#define ROJO "\x1B[1;31m"
+#define NORMAL "\x1B[0m"
+#define AMARILLO "\x1B[1;33m"
+#define VERDE "\x1B[1;32m"
+#define AZUL "\x1B[1;34m"
+#define NEGRITA "\x1B[1m"
+#define AZULETE "\x1B[1;36m"
+
+int main(){
+
+  double n;
+
+  printf(AZULETE "Introduce el número deseado: " NORMAL);
+  scanf("%lf", &n);
+
+  if(n>0){
+  printf("+---------------------------------------------------------------------+\n");
+  n = sqrt(n);
+  printf(AMARILLO "El resultado de la raíz cuadrada es: " NORMAL
+  NEGRITA "%.2f\n\n" NORMAL,n);
+}else{printf(ROJO "Introduce números positivos\n" NORMAL);}
+
+  return EXIT_SUCCESS;
+}
