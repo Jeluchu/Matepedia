f9f019b6510cd0a5f2c1b6ba9f94a13c9213bf28
Jesús Mª <jeluchu@gmail.com>
2018-02-02 09:18:26 +0100

    NEW AREAS & ADD CUADRADO

---
+++ 05_areas/cuadrado.cpp
@@ -0,0 +1,34 @@
+
+//PROGRAMA EN C++
+//AUTOR: JESÚS MARÍA CALDERÓN - GITHUB JELUCHU
+//https://github.com/Jeluchu
+
+// LIBRERÍAS
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <complex.h>
+
+
+// DEFINICIÓN
+#define ROJO "\x1B[1;31m"
+#define NORMAL "\x1B[0m"
+#define AMARILLO "\x1B[1;33m"
+#define VERDE "\x1B[1;32m"
+#define AZUL "\x1B[1;34m"
+#define NEGRITA "\x1B[1m"
+#define AZULETE "\x1B[1;36m"
+
+int main(){
+
+  float lado, area;
+
+  printf(AZULETE "Introduce la longitud del lado: " NORMAL);
+  scanf(" %f", &lado);
+
+  printf("+---------------------------------------------------------------------+\n");
+  area = lado*lado;
+  printf(AMARILLO "El resultado del área es: " NORMAL NEGRITA "%.2f\n" NORMAL, area);
+
+  return EXIT_SUCCESS;
+}
